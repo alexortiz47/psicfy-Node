413620645ffda34f917ce90399df42a8e3be5f2a
Alejandro Ortiz <ortizalex.47@gmail.com>
2019-03-21 10:25:43 +0100

    Cambio base de datos produccion

--- db.js
+++ db.js
@@ -3,12 +3,20 @@ const mysql = require('mysql');
 let pool = null;
 
 let connect = (done) => { 
+    // pool = mysql.createPool({
+    //     host: '127.0.0.1',
+    //     user: 'root',
+    //     password: 'root',
+    //     port: 8889,
+    //     database: 'psicfy'
+    // })
+
     pool = mysql.createPool({
-        host: '127.0.0.1',
-        user: 'root',
-        password: 'root',
-        port: 8889,
-        database: 'psicfy'
+        host: 'localhost',
+        user: 'web',
+        password: 'toribios947',
+        port: 3306,
+        database: 'psicfyco_production'
     })
     done()
 }
